058983e7d5340c8e83ee0dab8c7b967452ee9786
wminaung <minwin243@gmail.com>
2023-12-15 22:54:28 +0630

    First Commited -v1.0

---
+++ UtubeDL/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace UtubeDL
+{
+    partial class FmUtubeDL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmUtubeDL));
+            this.labelURL = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.sbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(44, 111);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(98, 17);
+            this.labelURL.TabIndex = 0;
+            this.labelURL.Text = "YouTube URL";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(204, 108);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(372, 22);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.TextChanged += new System.EventHandler(this.textBoxURL_TextChanged);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(217, 234);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(201, 52);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // FmUtubeDL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(643, 336);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.labelURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FmUtubeDL";
+            this.Text = "UtubeDL";
+            this.Load += new System.EventHandler(this.UtubeDL_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.FolderBrowserDialog sbd;
+    }
+}
+
---
+++ UtubeDL/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UtubeDL
+{
+    public partial class FmUtubeDL : Form
+    {   
+
+        private bool isDownloaded { get; set; } 
+        
+       
+
+        public FmUtubeDL()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void UtubeDL_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+          isDownloaded = false;
+          string URL = textBoxURL.Text;
+
+             if (URL.Trim().Length == 0 || URL.Length < 4)
+             {
+                 MessageBox.Show("You need to add URL from youtube");
+                 return;
+             };
+           
+            if (sbd.ShowDialog() == DialogResult.OK)
+            {
+               string selectedPath = sbd.SelectedPath;
+               btnDownload.Enabled = false;         
+               Console.WriteLine("select: " + selectedPath);
+               downloadFromYTDl(URL,selectedPath);
+                
+                if(isDownloaded)
+                {
+                    MessageBox.Show("Successfully downloaded");
+                    textBoxURL.Text = "";
+                    isDownloaded = false;
+                }
+                else
+                {
+                    MessageBox.Show("SomethingWrong! Please enter a valid url");
+
+                }
+                btnDownload.Enabled = true;
+
+            }
+
+
+        }
+
+
+        private  void downloadFromYTDl(string videoUrl ,string selectedPath)
+        {
+            try
+            {
+             
+                string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+                string ytdlExePath = baseDir + "yt-dlp.exe";
+                Console.WriteLine(ytdlExePath);
+                string userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+                 
+                string outputDir = $"{selectedPath}\\%(title)s.%(ext)s";
+                Console.WriteLine($"outputDir: {outputDir}");
+                // Check if yt-dlp executable exists
+                if (System.IO.File.Exists(ytdlExePath))
+                {
+                    // Start yt-dlp process with the desired command
+                    using (Process process = new Process())
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = ytdlExePath,
+                            Arguments = $"-o {outputDir}  {videoUrl}",
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                        };
+
+                        process.StartInfo = startInfo;
+                        process.Start();
+
+                        // Read the output and error streams
+                        string output = process.StandardOutput.ReadToEnd();
+                        string error = process.StandardError.ReadToEnd();
+
+                        process.WaitForExit();
+                        Console.WriteLine("Output: " + output);
+                        Console.WriteLine("Error: " + error);
+                        if(error != "")
+                        {
+                            isDownloaded = false;
+                        }
+                        else
+                        {
+                            isDownloaded = true;
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("yt-dlp not found. You need to download yt-dlp yourself. but don't worry I don't help you ");
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                Console.WriteLine($"Error: {ex.Message}"); 
+                MessageBox.Show("download too long");
+            }
+          
+        }
+
+       
+
+    
+
+        private void textBoxURL_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }// class
+}
